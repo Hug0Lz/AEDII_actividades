74766e12b181c574796d8fa88724598704c507b4
pgherrera17 <pgherrera17@esei.uvigo.es>
2025-10-31 18:43:44 +0000

    Implement buildGraph method for related words

--- activities2-master/src/main/java/es/uvigo/esei/aed2/activity6/relatedwords/RelatedWords.java
+++ activities2-master/src/main/java/es/uvigo/esei/aed2/activity6/relatedwords/RelatedWords.java
@@ -27,19 +27,76 @@ package es.uvigo.esei.aed2.activity6.relatedwords;
  * #L%
  */
 
+import es.uvigo.esei.aed2.activity6.HashMap.HashMap;
+import es.uvigo.esei.aed2.activity6.mapofmap.MapOfMap;
 import java.util.List;
-
 import es.uvigo.esei.aed2.graph.Graph;
+import es.uvigo.esei.aed2.graph.Vertex;
+import es.uvigo.esei.aed2.map.Map;
+import java.util.ArrayList;
+
 
 public class RelatedWords {
-  /**
-   * Construye un grafo que representa las relaciones entre las palabras.
-   *  
-   * @param words palabras a relacionar.
-   * @return grafo de palabras relacionadas.
-   */
-  public static Graph<String, Integer> buildGraph(List<String> words) {
-    // TODO: Implementa la construcción del grafo de palabras relacionadas
-    return null;
-  }
+
+    /**
+     * Construye un grafo que representa las relaciones entre las palabras.
+     *
+     * @param words palabras a relacionar.
+     * @return grafo de palabras relacionadas.
+     */
+    public static Graph<String, Integer> buildGraph(List<String> words) {
+        // TODO: Implementa la construcción del grafo de palabras relacionadas
+
+  Graph<String, Integer> graph = new MapOfMap<>();  
+  
+    if (words == null || words.isEmpty()) {
+        return graph;
+    }
+    
+    // 1. Añadir vértices
+    for (String word : words) {
+        graph.addVertex(new Vertex<>(word));
+    }
+    
+    // 2. Crear diccionario de patrones
+    Map<String, List<String>> diccionario = llenarMapa(words);
+
+    // 3. Conectar vértices (forma simple sin verificar duplicados)
+    for (String key : diccionario.getKeys()) {
+        List<String> lista = diccionario.get(key);
+        
+        if (lista.size() > 1) {
+            // Conectar todos los pares posibles en esta lista
+            for (int i = 0; i < lista.size(); i++) {
+                for (int j = i + 1; j < lista.size(); j++) {
+
+                        graph.addEdge(new Vertex<>(lista.get(i)), new Vertex<>(lista.get(j)), 0);
+                        graph.addEdge(new Vertex<>(lista.get(j)), new Vertex<>(lista.get(i)), 0);
+                }
+            }
+        }
+    }
+    
+    return graph;
+    }
+private static Map<String, List<String>> llenarMapa(List<String> words) {
+
+           Map<String, List<String>> mapa = new HashMap<>();
+    
+    for (String word : words) {
+
+        
+        for (int i = 0; i < word.length(); i++) {
+            String k = word.substring(0, i) + "_" + word.substring(i + 1);
+            
+            List<String> lista = mapa.get(k);
+            if (lista == null) {
+                lista = new ArrayList<>();
+            }
+            lista.add(word);
+            mapa.add(k, lista);
+        }
+    }
+        return mapa;
+    }
 }
